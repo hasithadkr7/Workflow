c3029acc4da2608639cdfdac0beb3ca04ed600f7
hasitha <hasithadkr7@gmail.com>
2020-01-08 10:54:12 +0530

    update flo2d150 server with upload discharge handler

--- flo2d/flo2d_server_150m.py
+++ flo2d/flo2d_server_150m.py
@@ -3,19 +3,22 @@ from builtins import print
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 from urllib.parse import urlparse, parse_qs
-from raincelldat.gen_raincell import create_hybrid_raincell
-from curw_sim.gen_raincell_curw_sim import create_sim_hybrid_raincell
-from inflowdat.get_inflow import create_inflow
-from outflowdat.gen_outflow import create_outflow
-from flo2d.run_model import execute_flo2d_250m, flo2d_model_completed
-from waterlevel.upload_waterlevel import upload_waterlevels_curw
-from extract.extract_water_level_hourly_run import upload_waterlevels
+from raincell.gen_raincell import create_sim_hybrid_raincell
+from inflow.get_inflow import create_inflow
+from outflow.get_outflow import create_outflow
+from outflow.gen_outflow_old import create_outflow_old
+from run_model import execute_flo2d_150m, flo2d_model_completed
+from extract.extract_curw import upload_waterlevels_curw
+from extract.extract_curw_fcst import upload_waterlevels
+from extract.extract_discharge_hourly_run import upload_discharges
 from os.path import join as pjoin
 from datetime import datetime, timedelta
 
-HOST_ADDRESS = '10.138.0.4'
+# HOST_ADDRESS = '10.138.0.4'
+# To remove the exception "[WinError 10049] The requested address is not valid in its context"
 # HOST_ADDRESS = '0.0.0.0'
-HOST_PORT = 8078
+HOST_ADDRESS = '10.138.0.7'
+HOST_PORT = 8089
 
 
 def set_daily_dir(run_date, run_time):
@@ -35,7 +38,7 @@ class StoreHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         print('Handle GET request...')
         if self.path.startswith('/create-raincell'):
-            os.chdir(r"D:\flo2d_hourly")
+            os.chdir(r"C:\workflow\flo2d_150m")
             print('create-raincell')
             response = {}
             try:
@@ -47,7 +50,7 @@ class StoreHandler(BaseHTTPRequestHandler):
                 [backward] = query_components["backward"]
                 print('[run_date, run_time] : ', [run_date, run_time])
                 dir_path = set_daily_dir(run_date, run_time)
-                create_hybrid_raincell(dir_path, run_date, run_time, forward, backward)
+                #create_hybrid_raincell(dir_path, run_date, run_time, forward, backward)
                 response = {'response': 'success'}
             except Exception as e:
                 print(str(e))
@@ -58,7 +61,7 @@ class StoreHandler(BaseHTTPRequestHandler):
             self.wfile.write(str.encode(reply))
 
         if self.path.startswith('/create-sim-raincell'):
-            os.chdir(r"D:\flo2d_hourly")
+            os.chdir(r"C:\workflow\flo2d_150m")
             print('create-sim-raincell')
             response = {}
             try:
@@ -68,10 +71,12 @@ class StoreHandler(BaseHTTPRequestHandler):
                 [run_time] = query_components["run_time"]
                 [forward] = query_components["forward"]
                 [backward] = query_components["backward"]
+                print('[forward, backward] : ', [forward, backward])
                 print('[run_date, run_time] : ', [run_date, run_time])
+                duration_days = (int(backward), int(forward))
                 dir_path = set_daily_dir(run_date, run_time)
-                create_sim_hybrid_raincell(dir_path, run_date, run_time, forward, backward,
-                                           res_mins=5, flo2d_model='flo2d_250',
+                create_sim_hybrid_raincell(dir_path, run_date, run_time, duration_days[1], duration_days[0],
+                                           res_mins=5, flo2d_model='flo2d_150',
                                            calc_method='MME')
                 response = {'response': 'success'}
             except Exception as e:
@@ -83,7 +88,7 @@ class StoreHandler(BaseHTTPRequestHandler):
             self.wfile.write(str.encode(reply))
 
         if self.path.startswith('/create-inflow'):
-            os.chdir(r"D:\flo2d_hourly")
+            os.chdir(r"C:\workflow\flo2d_150m")
             print('create-inflow')
             response = {}
             try:
@@ -91,10 +96,11 @@ class StoreHandler(BaseHTTPRequestHandler):
                 print('query_components : ', query_components)
                 [run_date] = query_components["run_date"]
                 [run_time] = query_components["run_time"]
+                [forward] = query_components["forward"]
+                [backward] = query_components["backward"]
+                print('[forward, backward] : ', [forward, backward])
                 print('[run_date, run_time] : ', [run_date, run_time])
                 dir_path = set_daily_dir(run_date, run_time)
-                backward = '2'
-                forward = '3'
                 duration_days = (int(backward), int(forward))
                 ts_start_date = datetime.strptime(run_date, '%Y-%m-%d') - timedelta(days=duration_days[0])
                 ts_end_date = datetime.strptime(run_date, '%Y-%m-%d') + timedelta(days=duration_days[1])
@@ -115,7 +121,7 @@ class StoreHandler(BaseHTTPRequestHandler):
             self.wfile.write(str.encode(reply))
 
         if self.path.startswith('/create-outflow'):
-            os.chdir(r"D:\flo2d_hourly")
+            os.chdir(r"C:\workflow\flo2d_150m")
             print('create-outflow')
             response = {}
             try:
@@ -146,8 +152,40 @@ class StoreHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(str.encode(reply))
 
+        if self.path.startswith('/create-outflow-old'):
+            os.chdir(r"C:\workflow\flo2d_150m")
+            print('create-outflow')
+            response = {}
+            try:
+                query_components = parse_qs(urlparse(self.path).query)
+                print('query_components : ', query_components)
+                [run_date] = query_components["run_date"]
+                [run_time] = query_components["run_time"]
+                [forward] = query_components["forward"]
+                [backward] = query_components["backward"]
+                print('[run_date, run_time] : ', [run_date, run_time])
+                dir_path = set_daily_dir(run_date, run_time)
+                duration_days = (int(backward), int(forward))
+                ts_start_date = datetime.strptime(run_date, '%Y-%m-%d') - timedelta(days=duration_days[0])
+                ts_end_date = datetime.strptime(run_date, '%Y-%m-%d') + timedelta(days=duration_days[1])
+                ts_end_date = ts_end_date.strftime('%Y-%m-%d')
+                ts_start_date = ts_start_date.strftime('%Y-%m-%d')
+                ts_start_time = '00:00:00'
+                ts_start = '{} {}'.format(ts_start_date, ts_start_time)
+                ts_end = '{} {}'.format(ts_end_date, ts_start_time)
+                print('create_outflow-[ts_start, ts_end]', [ts_start, ts_end])
+                create_outflow_old(dir_path, ts_start, ts_end)
+                response = {'response': 'success'}
+            except Exception as e:
+                print(str(e))
+            reply = json.dumps(response)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/json')
+            self.end_headers()
+            self.wfile.write(str.encode(reply))
+
         if self.path.startswith('/run-flo2d'):
-            os.chdir(r"D:\flo2d_hourly")
+            os.chdir(r"C:\workflow\flo2d_150m")
             print('run-flo2d')
             response = {}
             try:
@@ -157,7 +195,7 @@ class StoreHandler(BaseHTTPRequestHandler):
                 [run_time] = query_components["run_time"]
                 print('[run_date, run_time] : ', [run_date, run_time])
                 dir_path = set_daily_dir(run_date, run_time)
-                execute_flo2d_250m(dir_path, run_date, run_time)
+                execute_flo2d_150m(dir_path, run_date, run_time)
                 response = {'response': 'success'}
             except Exception as e:
                 print(str(e))
@@ -168,7 +206,7 @@ class StoreHandler(BaseHTTPRequestHandler):
             self.wfile.write(str.encode(reply))
 
         if self.path.startswith('/flo2d-completed'):
-            os.chdir(r"D:\flo2d_hourly")
+            os.chdir(r"C:\workflow\flo2d_150m")
             print('flo2d-completed')
             response = {}
             try:
@@ -190,8 +228,8 @@ class StoreHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(str.encode(reply))
 
-        if self.path.startswith('/extract-data'):
-            os.chdir(r"D:\flo2d_hourly")
+        if self.path.startswith('/extract-curw-fcst'):
+            os.chdir(r"C:\workflow\flo2d_150m")
             print('extract-data')
             response = {}
             try:
@@ -200,25 +238,27 @@ class StoreHandler(BaseHTTPRequestHandler):
                 [run_date] = query_components["run_date"]
                 [run_time] = query_components["run_time"]
                 dir_path = set_daily_dir(run_date, run_time)
-                backward = '2'
-                forward = '3'
+                [forward] = query_components["forward"]
+                [backward] = query_components["backward"]
                 duration_days = (int(backward), int(forward))
                 ts_start_date = datetime.strptime(run_date, '%Y-%m-%d') - timedelta(days=duration_days[0])
                 ts_start_date = ts_start_date.strftime('%Y-%m-%d')
                 ts_start_time = '00:00:00'
-                # upload_waterlevels_curw(dir_path, ts_start_date, ts_start_time)
                 upload_waterlevels(dir_path, ts_start_date, ts_start_time, run_date, run_time)
+                upload_discharges(dir_path, ts_start_date, ts_start_time, run_date, run_time)
                 response = {'response': 'success'}
             except Exception as e:
                 print(str(e))
-            reply = json.dumps(response)
-            self.send_response(200)
-            self.send_header('Content-type', 'text/json')
-            self.end_headers()
-            self.wfile.write(str.encode(reply))
+
+        reply = json.dumps(response)
+        self.send_response(200)
+        self.send_header('Content-type', 'text/json')
+        self.end_headers()
+        self.wfile.write(str.encode(reply))
+
 
         if self.path.startswith('/extract-curw'):
-            os.chdir(r"D:\flo2d_hourly")
+            os.chdir(r"C:\workflow\flo2d_150m")
             print('extract-data')
             response = {}
             try:
@@ -226,9 +266,9 @@ class StoreHandler(BaseHTTPRequestHandler):
                 print('query_components : ', query_components)
                 [run_date] = query_components["run_date"]
                 [run_time] = query_components["run_time"]
+                [forward] = query_components["forward"]
+                [backward] = query_components["backward"]
                 dir_path = set_daily_dir(run_date, run_time)
-                backward = '2'
-                forward = '3'
                 duration_days = (int(backward), int(forward))
                 ts_start_date = datetime.strptime(run_date, '%Y-%m-%d') - timedelta(days=duration_days[0])
                 ts_start_date = ts_start_date.strftime('%Y-%m-%d')
@@ -248,7 +288,6 @@ class StoreHandler(BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/json')
             self.end_headers()
             self.wfile.write(str.encode(reply))
-
 
 if __name__ == '__main__':
     try:
